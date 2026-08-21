39abc082b68a76a19925816f0e6e871fbdc1df6a
Georgewill Hope <georgeblue222@gmail.com>
2026-08-21 03:13:36 +0100

    Created craps game js module

---
+++ crapsgame/crapsgame.js
@@ -0,0 +1,18 @@
+// HTML Element IDs
+const crapsUsernameInput = "craps-username-input"
+const crapsRegistrationPane = "craps-registration-pane"
+const crapsMainSection = "craps-main-section"
+
+function registerCrapsPlayer() {
+    let crapsUsername = alert(document.getElementById(crapsUsernameInput).value)
+    removeRegistrationPane()
+    showMainGameSection()
+}
+
+function removeRegistrationPane() {
+    document.getElementById(crapsRegistrationPane).style.display = "none"
+}
+
+function showMainGameSection() {
+        document.getElementById(crapsMainSection).style.display = "block"
+}
